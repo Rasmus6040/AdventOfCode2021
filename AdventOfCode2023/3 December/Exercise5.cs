e2dcb38f8341d3a8b117740e6efd548e797a801f
Rasmus Pedersen <rasmus.pedersen@boostplm.com>
2023-12-03 10:35:36 +0100

    3 december

--- AdventOfCode2023/3 December/Exercise5.cs
+++ AdventOfCode2023/3 December/Exercise5.cs
@@ -6,6 +6,160 @@ public class Exercise5
 
     public int GetResult()
     {
-        return 1;
-    }    
+        var sum = 0;
+        for (var i = 0; i < _puzzleInput.Length; i++)
+        {
+            for (var j = 0; j < _puzzleInput.First().Length; j++)
+            {
+                if (!BadWayOfCheckingForSymbol(i, j, out var value)) continue;
+                j += value.ToString().Length;
+                sum += value;
+            }
+        }
+
+        return sum;
+    }
+
+    private bool BadWayOfCheckingForSymbol(int i, int j, out int number)
+    {
+        var hasSpecialChar = false;
+        // Check i-1, i, i+1 and for j values: j-1, j, j+1, j+2, j+3
+        if (j + 2 < _puzzleInput.First().Length && 
+            int.TryParse(_puzzleInput[i].AsSpan(j, 3), out number))
+        {
+            hasSpecialChar = SpecialCharNearNumber(i, j, 3);
+        }
+        else if (j + 1 < _puzzleInput.First().Length && 
+            int.TryParse(_puzzleInput[i].AsSpan(j, 2), out number))
+        {
+            hasSpecialChar = SpecialCharNearNumber(i, j, 2);
+        }
+        else if (j < _puzzleInput.First().Length && 
+                 int.TryParse(_puzzleInput[i].AsSpan(j, 1), out number))
+        {
+            hasSpecialChar = SpecialCharNearNumber(i, j, 1);
+        }
+        else
+        {
+            number = 0;
+        }
+        
+        return hasSpecialChar;
+    }
+
+    private bool SpecialCharNearNumber(int i, int j, int len)
+    {
+        var hasSpecialChar = false;
+        if (i != 0 && i < _puzzleInput.Length - 1 && j != 0 && j + len < _puzzleInput.First().Length)
+        {
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i - 1].Substring(j - 1, len+2)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i].Substring(j - 1, 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i].Substring(j + len, 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i + 1].Substring(j - 1, len + 2)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+        }
+        else if (i == 0 && j != 0 && j + len < _puzzleInput.First().Length)
+        {
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i].Substring(j - 1, 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i].Substring(j + len, 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+            hasSpecialChar = hasSpecialChar || _puzzleInput[1].Substring(j - 1, len + 2)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+        }
+        else if (i == _puzzleInput.Length - 1 && j != 0 && j + len < _puzzleInput.First().Length)
+        {
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i - 1].Substring(j - 1, len + 2)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i].Substring(j - 1, 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i].Substring(j + len, 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+        }
+        else if (i != 0 && i < _puzzleInput.Length - 1 && j == 0)
+        {
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i - 1].Substring(j, len + 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i].Substring(len, 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i + 1].Substring(j, len + 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+        }
+        else if (i != 0 && i < _puzzleInput.Length - 1 && j + len == _puzzleInput.First().Length)
+        {
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i - 1].Substring(j - 1, len + 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i].Substring(j - 1, 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i + 1].Substring(j - 1, len + 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+        }
+        else if (i == 0 && j == 0)
+        {
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i].Substring(len, 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+            hasSpecialChar = hasSpecialChar || _puzzleInput[1].Substring(j, len + 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+        }
+        
+        if (i != 0 && j != 0 && j + len < _puzzleInput.First().Length)
+        {
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i - 1].Substring(j - 1, len + 2)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+        }
+        else if (i != 0 && j == 0)
+        {
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i - 1].Substring(j, len + 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+        }
+        else if (i != 0 && j + len == _puzzleInput.First().Length)
+        {
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i - 1].Substring(j - 1, len + 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+        }
+        else if (j != 0 && j + len < _puzzleInput.First().Length)
+        {
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i].Substring(j - 1, 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i].Substring(j + len, 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+        }
+        else if (j + len == _puzzleInput.First().Length)
+        {
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i].Substring(j - 1, 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+        }
+        else if (j == 0)
+        {
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i].Substring(len, 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+        }
+        if (i < _puzzleInput.Length - 1 && j != 0 && j + len < _puzzleInput.First().Length)
+        {
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i + 1].Substring(j - 1, len + 2)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+        }
+        else if (i < _puzzleInput.Length - 1 && j == 0)
+        {
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i + 1].Substring(j, len + 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+        }
+        else if (i < _puzzleInput.Length - 1 && j + len == _puzzleInput.First().Length)
+        {
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i + 1].Substring(j - 1, len + 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+        }
+        else if (i == _puzzleInput.Length - 1 && j + len == _puzzleInput.First().Length)
+        {
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i - 1].Substring(j - 1, len + 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+            hasSpecialChar = hasSpecialChar || _puzzleInput[i].Substring(j - 1, 1)
+                .Any(ch => !char.IsLetterOrDigit(ch) && ch != '.');
+        }
+
+        return hasSpecialChar;
+    }
 }
--- AdventOfCode2023/3 December/Exercise6.cs
+++ AdventOfCode2023/3 December/Exercise6.cs
@@ -3,9 +3,95 @@ namespace AdventOfCode2023._3_December;
 public class Exercise6
 {
     private readonly string[] _puzzleInput = PuzzleInput.Get();
+    private readonly Dictionary<(int,int), int> _position = new ();
 
     public int GetResult()
     {
-        return 1;
-    }    
+        var sum = 0;
+        for (var i = 0; i < _puzzleInput.Length; i++)
+        {
+            for (var j = 0; j < _puzzleInput.First().Length; j++)
+            {
+                if (!BadWayOfCheckingForSymbol(i, j, out var value)) continue;
+                j += value.ToString().Length;
+            }
+        }
+
+        for(var i = 0; i < _puzzleInput.Length; i++)
+        {
+            for (var j = 0; j < _puzzleInput.First().Length; j++)
+            {
+                if (_puzzleInput[i][j] != '*') continue;
+                var firstNumber = FindFirstNumber(i, j);
+                if(firstNumber is null) continue;
+                sum += firstNumber.Value;
+            }
+        }
+
+        return sum;
+    }
+
+    private int? FindFirstNumber(int i, int j)
+    {
+        var foundCount = 0;
+        var accumulatedValue = 0;
+
+        // Directions: (deltaI, deltaJ, checkPreviousDeltaI, checkPreviousDeltaJ)
+        var directions = new (int, int, int?, int?)[] 
+        {
+            (-1, -1, null, null), (-1, 0, -1, -1), (-1, 1, -1, 0), 
+            (0, -1, null, null), (0, 1, null, null), 
+            (1, -1, null, null), (1, 0, 1, -1), (1, 1, 1, 0)
+        };
+
+        foreach (var (deltaI, deltaJ, prevDeltaI, prevDeltaJ) in directions)
+        {
+            if (_position.TryGetValue((i + deltaI, j + deltaJ), out var value))
+            {
+                // Check if it's the same number as in the previous direction
+                var isDifferentNumber = prevDeltaI == null || !_position.TryGetValue((i + prevDeltaI.Value, j + prevDeltaJ!.Value), out _);
+
+                if (isDifferentNumber)
+                {
+                    foundCount++;
+                    accumulatedValue = (accumulatedValue == 0) ? value : accumulatedValue * value;
+                }
+            }
+            
+            if (foundCount >= 2 && accumulatedValue != 0) return accumulatedValue;
+        }
+        return null;
+    }
+
+    private bool BadWayOfCheckingForSymbol(int i, int j, out int number)
+    {
+        var hasSpecialChar = false;
+        if (j + 2 < _puzzleInput.First().Length && 
+            int.TryParse(_puzzleInput[i].AsSpan(j, 3), out number))
+        {
+            _position.Add((i,j), number);
+            _position.Add((i,j+1), number);
+            _position.Add((i,j+2), number);
+            hasSpecialChar = true;
+        }
+        else if (j + 1 < _puzzleInput.First().Length && 
+            int.TryParse(_puzzleInput[i].AsSpan(j, 2), out number))
+        {
+            _position.Add((i,j), number);
+            _position.Add((i,j+1), number);
+            hasSpecialChar = true;
+        }
+        else if (j < _puzzleInput.First().Length && 
+                 int.TryParse(_puzzleInput[i].AsSpan(j, 1), out number))
+        {
+            _position.Add((i,j), number);
+            hasSpecialChar = true;
+        }
+        else
+        {
+            number = 0;
+        }
+        
+        return hasSpecialChar;
+    }
 }
