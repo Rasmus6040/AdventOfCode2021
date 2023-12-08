f7afcc719b9c0086b4ffd13d4da186eadccbeb46
Rasmus Pedersen <rasmus.pedersen@boostplm.com>
2023-12-08 08:13:55 +0100

    Day 8

--- AdventOfCode2023/8 December/Exercise15.cs
+++ AdventOfCode2023/8 December/Exercise15.cs
@@ -6,6 +6,26 @@ public class Exercise15
 
     public int GetResult()
     {
-        return 1;
+        var instructions = _puzzleInput.First();
+
+        var dict = new Dictionary<string, int>();
+        
+        _puzzleInput.Skip(2).Select((x, i) => (x, i))
+            .ToList().ForEach(tuple => dict.Add(tuple.x.Split(" = ").First(), tuple.i));
+
+
+
+        var previous = dict["AAA"];
+        var steps = 0;
+        while (true)
+        {
+            foreach (var direct in instructions.Select(direction => 'R' == direction ? 1 : 0))
+            {
+                var current = _puzzleInput[2 + previous].Split(" = (").Last().Split(")").First().Split(", ")[direct];
+                steps++;
+                if(current == "ZZZ") return steps;
+                previous = dict[current];
+            }   
+        }
     }    
 }
--- AdventOfCode2023/8 December/Exercise16.cs
+++ AdventOfCode2023/8 December/Exercise16.cs
@@ -4,8 +4,65 @@ public class Exercise16
 {
     private readonly string[] _puzzleInput = PuzzleInput.Get();
 
-    public int GetResult()
+    public long GetResult()
     {
-        return 1;
-    }    
+        var instructions = _puzzleInput.First().Select(direction => 'R' == direction ? 1 : 0).ToList();
+
+        var dict = new Dictionary<string, int>();
+        
+        _puzzleInput.Skip(2).Select((x, i) => (x, i))
+            .ToList().ForEach(tuple => dict.Add(tuple.x.Split(" = ").First(), tuple.i));
+        
+        var previous = _puzzleInput.Skip(2).Where(x => x.Split(" = ").First().Last() == 'A').Select(x => dict[x.Split(" = ").First()]).ToList();
+
+        var counts = new List<int>();
+        for (var i = 0; i < previous.Count; i++)
+        {
+            var foundResult = false;
+            var localCount = 0;
+            while (!foundResult)
+            {
+                foreach (var current in instructions.Select(t => _puzzleInput[2 + previous[i]].Split(" = (").Last().Split(")").First().Split(", ")[t]))
+                {
+                    previous[i] = dict[current];
+                    localCount++;
+                    if (!current.EndsWith("Z")) continue;
+                    
+                    counts.Add(localCount);
+                    foundResult = true;
+                    break;
+                }
+            }
+        }
+        return GetLcm(counts.ToArray());
+    }       
+    
+    // Function to find the LCM of an array of numbers
+    private static long GetLcm(IReadOnlyList<int> numbers)
+    {
+        long result = numbers[0];
+        for (var i = 1; i < numbers.Count; i++)
+        {
+            result = GetLcm(result, numbers[i]);
+        }
+        return result;
+    }
+
+    // Function to find LCM of two numbers
+    private static long GetLcm(long a, long b)
+    {
+        return a * b / GetGcd(a, b);
+    }
+
+    // Function to find GCD of two numbers
+    private static long GetGcd(long a, long b)
+    {
+        while (b != 0)
+        {
+            var temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
 }
